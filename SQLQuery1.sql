90653d01ea55c7c7fd5029736acd08cfa7321472
Agnita Paul <65063644+agnifiraz@users.noreply.github.com>
2022-05-10 01:47:18 -0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,54 @@
+﻿INSERT INTO Brands (Name)
+VALUES ("Iphone");
+INSERT INTO Brands (Name)
+VALUES ("Samsung");
+INSERT INTO Brands (Name)
+VALUES ("Xiaomi");
+INSERT INTO Brands (Name)
+VALUES ("Vivo");
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (1, "Iphone 13", "Iphone13.png", 1111, 100,2, 7, "New Iphone 13" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (1, "Iphone 12", "Iphone12.png", 1000, 500,4, 17, "old Iphone 12" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (1, "Iphone 11", "Iphone11.png", 999, 90,21, 7, "New Iphone 11" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (1, "Iphone13ProMax", "Iphone13.png", 1999, 199,20, 6, "New Iphone 13 Pro max" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (1, "Iphone 13", "Iphone13.png", 1222, 200,12, 7, "New Iphone 13" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (2, "Samsung M21", "SamsungM21.png", 2000, 200,22, 3, "New Samsung M21" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (2, "Samsung A21", "SamsungA21.png", 1000, 60,11, 17, "New Samsung A21" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (2, "Samsung Galaxy", "SamsungGalaxy.png", 2000, 220,27, 9, "New Samsung Galaxy" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (2, "Samsung Galaxy Pro", "SamsungGalaxy.png", 2222, 22,22, 27, "New Samsung Galaxy Pro" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (2, "Samsung Galaxy 1", "SamsungGalaxy.png", 999, 199,32, 8, "New Samsung Galaxy 1" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (3, "Xioami", "Xioami.png", 2222, 200,5, 9, "New Xioami" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (3, "Xioami7Pro", "Xioami7Pro.png", 666, 60,6, 66, "New Xioami Note 7 Pro" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (3, "Xioami8Pro", "Xioami8Pro.png", 999, 90,9, 8, "New Xioami Note 8 Pro" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (3, "Xioami9Pro", "Xioami9Pro.png", 2222, 234,4, 1, "New Xioami Note 9 Pro" );
+
+INSERT INTO Products (BrandId, ProductName, GraphicName, CostPrice, MSRP, QtyOnHand, QtyOnBackOrder, Description)
+VALUES (3, "Xioami10Pro", "Xioami10Pro.png", 1234, 123,12, 21, "New Xioami Note 10 Pro" );
+
